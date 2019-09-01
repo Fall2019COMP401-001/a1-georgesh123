b22a89476fc4471c0daafa66d3f83904d8662fd7
georgesh <georgesh@192.168.0.8>
2019-09-01 05:01:09 -0400

    added without comments

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,58 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		// Number of people in list
+		int count = scan.nextInt();
 		
+		Shopper[] listOfShoppers = new Shopper[count];
+		
+		for (int i = 0; i < count; i++) {
+			listOfShoppers[i].first = scan.next().charAt(0);
+			listOfShoppers[i].last = scan.next();
+			listOfShoppers[i].numOfItems = scan.nextInt();
+			listOfShoppers[i].listOfItems = new Items[listOfShoppers[i].numOfItems];
+			for (int j = 0; j < listOfShoppers[i].numOfItems; j++) {
+				listOfShoppers[i].listOfItems[j].quant = scan.nextInt();
+				listOfShoppers[i].listOfItems[j].nameOfItem = scan.next();
+				listOfShoppers[i].listOfItems[j].price = scan.nextDouble();
+			}
+		}
+		double[] pricePerPerson = new double[count];
+		
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j < listOfShoppers[i].numOfItems; j++) {
+				double tot = 0;
+				double totPerItem =  listOfShoppers[i].listOfItems[j].quant * listOfShoppers[i].listOfItems[j].price;
+				pricePerPerson[i] = pricePerPerson[i] + totPerItem;
+			}
+		}
+		
+		double[] averagePerPerson = new double[count];
+		for (int i = 0; i < count; i++) {
+			averagePerPerson[i] = pricePerPerson[i] / listOfShoppers[i].numOfItems;
+		}
+		
+		for (int i = 0; i < count; i++) {
+			System.out.println(listOfShoppers[i].first + ". " + listOfShoppers[i].last + " " + averagePerPerson[i]);
+		}
+		
+	}
+	
+	/* This creates a class for each shopper.
+	 * It includes their first initial and last name, number of items, and a string of items.
+	 */
+	public class Shopper {
+		char first;
+		String last;
+		int numOfItems;
+		Items[] listOfItems;
+	}
+	/* This creates a class for each item.
+	 * It includes the quantity, name, and price of each item.
+	 */
+	public class Items {
+		int quant;
+		String nameOfItem;
+		double price;
 	}
 }
