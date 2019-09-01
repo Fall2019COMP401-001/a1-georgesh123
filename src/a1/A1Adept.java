80fa0c18778fd12c59e44d0536e506e918c80459
georgesh <georgesh@192.168.0.8>
2019-09-01 18:14:09 -0400

    maybe got it

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -9,13 +10,20 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int itemsInStore = scan.nextInt();
+		String[] listOfItems = new String[itemsInStore];
+		double[] listOfPrices = new double[itemsInStore];
+		for (int i = 0; i < itemsInStore; i++) {
+			listOfItems[i] = scan.next();
+			listOfPrices[i] = scan.nextDouble();
+		}
+		
 		int count = scan.nextInt();
 		
 		String[] first = new String[count];
 		String[] last = new String[count];
 		int[] numOfItems = new int[count];
-		double[] priceOfItems = new double[count];
-		double[] totPrices = new double[count];
+		double[] pricePerPerson = new double[count];
 		for (int i = 0; i < count; i++) {
 			first[i] = scan.next();
 			last[i] = scan.next();
@@ -23,32 +31,44 @@ public class A1Adept {
 			for (int j = 0; j < numOfItems[i]; j++) {
 				int numPerItem = scan.nextInt();
 				String name = scan.next();
-				double price = scan.nextDouble() * numPerItem;
-				totPrices[i] = totPrices[i] + price;
+				double price = 0;
+				for (int k = 0; k < itemsInStore; k++) {
+					if (name.equals(listOfItems[k])) {
+						price = listOfPrices[k];
+					}
+				}
+				price = price * numPerItem;
+				pricePerPerson[i] = pricePerPerson[i] + price;
 			}
 		}
+		
 		scan.close();
 		int biggest = 0;
 		for (int i = 0; i < count; i++) {
-			if (totPrices[i] > totPrices[biggest]) {
+			if (pricePerPerson[i] > pricePerPerson[biggest]) {
 				biggest = i;
 			}
 		}
+		String biggestTot = String.format("%.2f", pricePerPerson[biggest]);
+		
 		int smallest = 0;
 		for (int i = 0; i < count; i++) {
-			if (totPrices[i] < totPrices[smallest]) {
+			if (pricePerPerson[i] <pricePerPerson[smallest]) {
 				smallest = i;
 			}
 		}
+		String smallestTot = String.format("%.2f", pricePerPerson[smallest]);
+		
 		double totPrice = 0;
 		for (int i = 0; i < count; i++) {
-			totPrice = totPrice + totPrices[i];
+			totPrice += pricePerPerson[i];
 		}
 		double average = totPrice / count;
 		String averagePrice = String.format("%.2f", average);
 		
-		System.out.println("Biggest: " + first[biggest] + " " + last[biggest] + " (" + totPrices[biggest] + ")");
-		System.out.println("Smallest: " + first[smallest] + " " + last[smallest] + " (" + totPrices[smallest] + ")");
+		System.out.println("Biggest: " + first[biggest] + " " + last[biggest] + " (" + biggestTot + ")");
+		System.out.println("Smallest: " + first[smallest] + " " + last[smallest] + " (" + smallestTot + ")");
 		System.out.println("Average: " + averagePrice);
+		
 	}
 }
