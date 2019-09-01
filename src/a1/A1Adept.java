93924e3fb8aaebcccb4c716d40553634056869d1
georgesh <georgesh@192.168.0.8>
2019-09-01 16:01:13 -0400

    First pass

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,46 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
 		
+		String[] first = new String[count];
+		String[] last = new String[count];
+		int[] numOfItems = new int[count];
+		double[] priceOfItems = new double[count];
+		double[] totPrices = new double[count];
+		for (int i = 0; i < count; i++) {
+			first[i] = scan.next();
+			last[i] = scan.next();
+			numOfItems[i] = scan.nextInt();
+			for (int j = 0; j < numOfItems[i]; j++) {
+				int numPerItem = scan.nextInt();
+				String name = scan.next();
+				double price = scan.nextDouble() * numPerItem;
+				totPrices[i] = totPrices[i] + price;
+			}
+		}
+		scan.close();
+		int biggest = 0;
+		for (int i = 0; i < count; i++) {
+			if (totPrices[i] > totPrices[biggest]) {
+				biggest = i;
+			}
+		}
+		int smallest = 0;
+		for (int i = 0; i < count; i++) {
+			if (totPrices[i] < totPrices[smallest]) {
+				smallest = i;
+			}
+		}
+		double totPrice = 0;
+		for (int i = 0; i < count; i++) {
+			totPrice = totPrice + totPrices[i];
+		}
+		double average = totPrice / count;
+		String averagePrice = String.format("%.2f", average);
+		
+		System.out.println("Biggest: " + first[biggest] + " " + last[biggest] + " (" + totPrices[biggest] + ")");
+		System.out.println("Smallest: " + first[smallest] + " " + last[smallest] + " (" + totPrices[smallest] + ")");
+		System.out.println("Average: " + averagePrice);
 	}
 }
